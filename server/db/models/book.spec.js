9c8febfe4a12f4f3692f96635de5bf051aa4fdcf
Jing Lu <jl201835@gmail.com>
2019-01-08 15:47:18 -0500

    Validate author not null

--- server/db/models/book.spec.js
+++ server/db/models/book.spec.js
@@ -10,20 +10,35 @@ describe('Book model', () => {
   describe('Validations', () => {
     let book
 
-    before(() => {
+    beforeEach(() => {
       book = Book.build()
     })
 
     it('requires `title`', async () => {
+      //book.title = 'Harry Potter'
       book.author = 'Scott Peck'
       book.price = 10.99
       book.inventory = 2
 
       try {
         await book.validate()
-        throw Error('validate succeeded but should have failed without `name`')
+        throw new Error('validate succeeded but should have failed')
       } catch (err) {
         expect(err.message).to.contain('title')
+      }
+    })
+
+    it('requires `author`', async () => {
+      book.title = 'Harry Potter'
+      //book.author = 'Scott Peck'
+      book.price = 10.99
+      book.inventory = 2
+
+      try {
+        await book.validate()
+        throw new Error('validate succeeded but should have failed')
+      } catch (err) {
+        expect(err.message).to.contain('author')
       }
     })
   })
