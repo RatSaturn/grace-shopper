b3645c0cde1c4feb9634c2be05827bb6a80e8c13
Sher-Min Yang <sherminyang@gmail.com>
2019-01-09 11:45:08 -0500

    refactor: replaced dummy data with backend API using redux, refactored header into link to landing page

--- client/components/BookComponent.js
+++ client/components/BookComponent.js
@@ -2,11 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const BookComponent = props => {
-  const {id, img, title, author, price, format} = props.book
+  const {id, imageUrl, title, author, price, format} = props.book
 
   return (
     <div>
-      <img src={img} />
+      <img src={imageUrl} />
       <p>
         <Link exact to={`/allbooks/${id}`}>
           {title}
