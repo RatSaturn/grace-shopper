c8587a60840926440c8059b978e8716bbf8e15ac
Sher-Min Yang <sherminyang@gmail.com>
2019-01-11 14:57:26 -0500

    chore: wrote findAllOrders class method

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -23,3 +23,27 @@ Order.findAllOrders = async function() {
   )
   return allOrders
 }
+
+// Order.update
+/*
+  Order.update(id, {order update object}) 
+    update object will have bookId and quantity
+    if quantity is 0, remove book from order
+    otherwise, update quantity with quantity from object
+    return updated order
+*/
+
+/*
+Order.findOrder(id)
+  take order id (which is unique instance of somebody creating a cart), look in BooksForOrder, return array of objects with all books associated with order ID
+
+
+Order.getAll()
+  load all orders in Order table, with associated books eager-loaded
+
+*/
+
+// Instance Method
+// order.update(bookId, quantity)
+// 	add to BooksForOrder (insert, delete, update entries)
+// 	if quantity is 0, delete
