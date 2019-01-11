31c4f5cbb5bd329c4edc232ff280018640b4c950
Jing Lu <jl201835@gmail.com>
2019-01-10 22:43:15 -0500

    seed 3 pending orders

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,11 +1,30 @@
 const router = require('express').Router()
-const {Order, Book} = require('../db/models')
+const {Order, User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const orders = await Order.findAll({include: [Book]})
+    const orders = await Order.getAll()
     res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/cart', async (req, res, next) => {
+  try {
+    // if cart doesn't exist, create one
+    // if user is logged in, add userId to order
+    // todo: when user first logged in, add userId to pending order
+    if (!req.session.cartId) {
+      const cart = Order.create()
+      req.session.cartId = cart.id
+      if (req.user) {
+        const user = User.findById(req.user)
+        cart.setUser(user)
+      }
+      res.status(200).json([])
+    }
   } catch (err) {
     next(err)
   }
