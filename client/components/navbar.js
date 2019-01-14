e5b186cafa594b4009d7b782656a1d8ca0563b0a
Sher-Min Yang <sherminyang@gmail.com>
2019-01-14 16:29:33 -0500

    feat: impelemented redirect on checkout

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -9,6 +9,7 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import SimpleMenu from './drop-down-menu'
+
 const styles = {
   root: {
     flexGrow: 1
