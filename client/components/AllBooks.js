c4510cd067f92827afd1f56f7d2878a7838d3b5c
Michelle <mcurena24@gmail.com>
2019-01-13 20:27:28 -0500

    feat: added loader in allBooks component

--- client/components/AllBooks.js
+++ client/components/AllBooks.js
@@ -2,11 +2,22 @@ import React, {Component} from 'react'
 import BookComponent from './BookComponent'
 import {connect} from 'react-redux'
 import {getBooksFromApi} from '../store'
+import {BeatLoader} from 'react-spinners'
 
 export class AllBooks extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true
+    }
+  }
+
   async componentDidMount() {
     try {
       await this.props.getBooksFromApi()
+      this.setState({
+        loading: false
+      })
     } catch (err) {
       console.error(err)
     }
@@ -18,6 +29,9 @@ export class AllBooks extends Component {
         <center>
           <h1>All Books</h1>
         </center>
+        <div className="loader">
+          <BeatLoader color="#708090" loading={this.state.loading} />
+        </div>
         <div className="allbookscontainer">
           {this.props.books.map(book => (
             <BookComponent
