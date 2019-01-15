ca534e7895f1e32c28697148db5af57d7b7ec567
Michelle <mcurena24@gmail.com>
2019-01-15 15:43:11 -0500

    fix: singlebook component styling

--- client/components/SingleBook.js
+++ client/components/SingleBook.js
@@ -2,11 +2,9 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getSingleBookFromApi, updateCartOnServer} from '../store'
 import Card from '@material-ui/core/Card'
-import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import CardActions from '@material-ui/core/CardActions'
-import Select from '@material-ui/core/Select'
 import {withStyles} from '@material-ui/core'
 import PropTypes from 'prop-types'
 import Grid from '@material-ui/core/Grid'
@@ -14,7 +12,10 @@ import Grid from '@material-ui/core/Grid'
 const styles = () => ({
   card: {
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    flex: '1 0 auto',
+    alignItems: 'center',
+    justify: 'center'
   },
   details: {
     display: 'flex',
@@ -28,15 +29,6 @@ const styles = () => ({
   },
   cover: {
     width: 151
-  },
-  controls: {
-    display: 'flex',
-    alignItems: 'center',
-    justify: 'flex-end'
-  },
-  playIcon: {
-    height: 38,
-    width: 38
   }
 })
 
@@ -59,68 +51,108 @@ export class SingleBook extends Component {
     }
 
     return (
-      <Grid
-        container
-        spacing={16}
-        style={{paddingLeft: '15%'}}
-        direction="column"
-        alignItems="stretch"
-        justify="space-between"
-        alignContent="stretch"
-      >
-        <Card className={classes.card} style={{width: '70vw'}}>
-          <Grid container>
-            <Grid item>
-              <CardMedia
-                className={classes.cover}
-                style={{height: '20vw'}}
-                image={singleBook.imageUrl}
-                title={singleBook.title}
-              />
-            </Grid>
-            <Grid item className={classes.details}>
-              <Grid item>
-                <CardContent className={classes.content}>
-                  <Typography gutterBottom variant="headline" component="h2">
-                    {singleBook.title}
-                  </Typography>
-                  <Typography component="p">
-                    {singleBook.author}
-                    <br />
-                    ${displayPrice}
-                  </Typography>
-                </CardContent>
+      <div>
+        <Grid
+          container
+          spacing={8}
+          style={{paddingLeft: '15%', marginTop: '2%'}}
+          direction="column"
+          alignItems="stretch"
+          justify="space-between"
+          alignContent="stretch"
+        >
+          <Grid item xs={12} sm={12} lg={12} xl={12}>
+            <Card
+              className={classes.card}
+              style={{width: '60vw', height: '40vw'}}
+            >
+              <Grid
+                container
+                className={classes.card}
+                spacing={8}
+                direction="column"
+                alignItems="stretch"
+                justify="space-between"
+                alignContent="stretch"
+              >
+                <Grid item xs={4} sm={4} lg={4} xl={4}>
+                  <center>
+                    <img
+                      src={singleBook.imageUrl}
+                      alt={singleBook.title}
+                      className="single-book-image"
+                    />
+                  </center>
+                </Grid>
+                <Grid item xs={7} sm={7} lg={7} xl={7}>
+                  <Grid
+                    container
+                    className={classes.details}
+                    spacing={16}
+                    direction="column"
+                    alignItems="center"
+                    justify="space-evenly"
+                    alignContent="stretch"
+                  >
+                    <Grid item xs={12} sm={12} lg={12} xl={12}>
+                      <CardContent className={classes.content}>
+                        <center>
+                          <Typography
+                            gutterBottom
+                            variant="headline"
+                            component="h2"
+                          >
+                            {singleBook.title}
+                          </Typography>
+                        </center>
+                        <center>
+                          {singleBook.authors &&
+                            singleBook.authors.map(author => (
+                              <Typography component="h4" key={author}>
+                                {author}
+                              </Typography>
+                            ))}
+                          <Typography component="h4">
+                            ${displayPrice}
+                          </Typography>
+                        </center>
+                      </CardContent>
+                    </Grid>
+                    <Grid item xs={12} sm={12} lg={12} xl={12}>
+                      <CardContent className={classes.content}>
+                        <Typography component="p">
+                          {singleBook.description}
+                        </Typography>
+                      </CardContent>
+                    </Grid>
+                    <Grid item xs={12} sm={12} lg={12} xl={12}>
+                      <CardActions className={classes.controls}>
+                        <Typography component="h5">
+                          <button
+                            type="button"
+                            onClick={() =>
+                              this.props.updateCartOnServer({
+                                bookId: singleBook.id,
+                                book: singleBook,
+                                quantity: 1
+                              })
+                            }
+                          >
+                            Add to Cart
+                          </button>
+                        </Typography>
+                      </CardActions>
+                    </Grid>
+                  </Grid>
+                </Grid>
+                <Grid item xs={1} sm={1} lg={1} xl={1}>
+                  {' '}
+                </Grid>
               </Grid>
-              <Grid item>
-                <CardContent className={classes.content}>
-                  <Typography component="p" style={{padding: '15%'}}>
-                    {singleBook.description}
-                  </Typography>
-                </CardContent>
-              </Grid>
-              <Grid item>
-                {' '}
-                <CardActions className={classes.controls}>
-                  <Typography component="h5">
-                    <button
-                      type="button"
-                      onClick={() =>
-                        this.props.updateCartOnServer({
-                          bookId: singleBook.id,
-                          book: singleBook,
-                          quantity: 1
-                        })
-                      }
-                    >
-                      Add to Cart
-                    </button>
-                  </Typography>
-                </CardActions>
-              </Grid>
-            </Grid>
+            </Card>
           </Grid>
-        </Card>
-      </Grid>
+        </Grid>
+      </div>
     )
   }
 }
